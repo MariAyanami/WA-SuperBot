55c157088fa8968a0404633f3888fbe086763cfe
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-02-09 19:22:07 +0000

    Update distmeta

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const axios = require("axios");
 const qrcode = require("qrcode-terminal");
-const mime = require("mime-types"
+const mime = require("mime-types");
+const path = require("path");
+const fs = require("fs");
+// Requi
