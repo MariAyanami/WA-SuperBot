1a6e57500d8dd4210ec1d45e37c2670af45660c2
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-02-13 05:37:24 +0000

    sunrpc: fix braino in ->poll()

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.API_KEY,
-})
+});
+const openai = new OpenAIApi(configuration);
+
+// Meme F
