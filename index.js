4d40917db8eed4ade9511335c4fbfa926b5c4de8
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-02-26 11:15:13 +0000

    code alignment

--- index.js
+++ index.js
@@ -84,4 +84,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
                 fs.writeFileSync(fullFilename, media.data, {
                   encoding: "base64",
                 });
-                console.log("File downloaded successfull
+                console.log("File downloaded successfully!", fullFilename);
+                console.log(fullFilenam
