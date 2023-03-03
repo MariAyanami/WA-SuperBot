b7e833b691f499777e723ff564163ed6a30d23b3
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-03 08:00:45 +0000

    Continue testing (steady state).

--- index.js
+++ index.js
@@ -95,4 +95,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
                     sendMediaAsSticker: true,
                   }
                 );
-       
+                fs.unlinkSync(fullFilename);
+          
