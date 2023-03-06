31052dc716b2b80dc0d1e3131cb1d21cf9edea63
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-06 20:17:10 +0000

    Update auth.js

--- index.js
+++ index.js
@@ -110,4 +110,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         const apiData = await axios.get("https://type.fit/api/quotes");
         const randomNumber = Math.floor(Math.random() * apiData.data.length);
         message.reply(`*${apiData.data[randomNumber].text}*`);
-      } else if (message.body
+      } else if (message.body === "-ping") {
+        return message.reply("pong"
