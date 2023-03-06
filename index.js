6ad5aadcf12e2e029f4850b2fc41bb1cffa74966
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-06 11:21:51 +0000

    More verbosity

--- index.js
+++ index.js
@@ -108,4 +108,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         }
       } else if (message.body === "-quote") {
         const apiData = await axios.get("https://type.fit/api/quotes");
-        const randomNumber = Math.floor(Math.random() * a
+        const randomNumber = Math.floor(Math.random() * apiData.data.length);
+        message.reply(`*${apiDat
