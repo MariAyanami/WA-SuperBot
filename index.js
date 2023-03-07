be496af704d02afb38aa05abac9999c8a3fab35a
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-07 18:48:49 +0000

    Some more tweaks

--- index.js
+++ index.js
@@ -127,4 +127,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
             prompt: `tell me ${prompt}`,
             temperature: 0.7,
             max_tokens: 3000,
-   
+            top_p: 1.0,
+            frequency_penalty: 0
