1e6056925c707adc50300221331d07874d7b55d5
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-11 13:50:56 +0000

    Update pip

--- index.js
+++ index.js
@@ -128,4 +128,6 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
             temperature: 0.7,
             max_tokens: 3000,
             top_p: 1.0,
-            frequency_penalty: 0
+            frequency_penalty: 0.2,
+            presence_penalty: 0,
+          });
