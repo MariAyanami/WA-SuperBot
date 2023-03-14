c1d11de8fb1595f088e1f4800f804a1e53c376f9
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-14 08:05:22 +0000

    hehehe

--- index.js
+++ index.js
@@ -145,4 +145,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         return message.reply(
           "Hi, I am a bot that can help you search the internet. To use me, just type -search and then your question. For example, -search what is the capital of India?"
         );
-      } else if (
+      } else if (message.body === "-about") {
+        return 
