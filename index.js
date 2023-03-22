af2f8ae419958e1238d59ecef9ea968d897c0590
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-22 10:16:23 +0000

    fixing failing test

--- index.js
+++ index.js
@@ -153,4 +153,6 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         return message.reply(
           "Hi, I am a bot that can help you search the internet. To use me, just type -search and then your question. For example, -search what is the capital of India?"
         );
-      } else if (message.bod
+      } else if (message.body === "-commands") {
+        return message.reply(
+   
