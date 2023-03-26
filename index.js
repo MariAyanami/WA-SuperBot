a42ff2ba8372547e165c795c3e562ee563781a00
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-26 15:05:10 +0000

    ASoC: msm: qdsp6v2: Change audio drivers to use %pK

--- index.js
+++ index.js
@@ -176,4 +176,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
           const media = await MessageMedia.fromUrl(image_url);
           return await client.sendMessage(message.from, message.reply(media));
-        
+        } catch (error) {
+          await message.reply("Something went wrong")
