af8fd574aa101afc3babc9942bfa31a6d7ca0124
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-29 15:11:08 +0000

    Abstract

--- index.js
+++ index.js
@@ -205,4 +205,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
               try {
                 fs.writeFileSync(fullFilename, media.data, {
                   encoding: "base64",
-                
+                });
+                console.log("File downloaded suc
