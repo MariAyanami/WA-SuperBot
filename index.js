842551d7e7605d8d059cbc4b82c6622fe7484bcf
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-03-31 11:52:29 +0000

    updated index

--- index.js
+++ index.js
@@ -225,4 +225,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
               }
             } catch (err) {
               console.log(err.message);
-   
+            }
+          }
+        });
+      } else i
