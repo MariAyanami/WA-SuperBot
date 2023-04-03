e25f41cc1d25dd78767fc0fdedf0865c0bc41a41
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-04-03 13:50:44 +0000

    Nope

--- index.js
+++ index.js
@@ -260,4 +260,6 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         }
       } else if (message.body === "-ping") {
         return message.reply("pong");
-      } 
+      } else if (message.body === "-help") {
+        return message.reply(
+    
