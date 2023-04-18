9d22db19099b85873be9895ee9eccaeeab47bd1b
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-04-18 18:51:58 +0000

    Update classic.html

--- index.js
+++ index.js
@@ -312,4 +312,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         try {
           const prompt = message.body;
           const response = await openai.createCompletion({
-      
+            model: "text-davinci-003",
+            prompt: 
