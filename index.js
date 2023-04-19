4dc16164fc5318b815c7efcbf090cc4ed1c02c46
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-04-19 21:56:34 +0000

    Linter changes

--- index.js
+++ index.js
@@ -315,4 +315,6 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
             model: "text-davinci-003",
             prompt: prompt,
             temperature: 0.7,
-            max_toke
+            max_tokens: 3000,
+            top_p: 1.0,
+            frequ
