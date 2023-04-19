be8c813f1bf1ad3b784e29fdfb58900ea177a8c4
MariAyanami <131362907+MariAyanami@users.noreply.github.com>
2023-04-19 23:01:24 +0000

    Update main.rs

--- index.js
+++ index.js
@@ -317,4 +317,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
             temperature: 0.7,
             max_tokens: 3000,
             top_p: 1.0,
-            frequ
+            frequency_penalty: 0.2,
+            presence_penalty:
